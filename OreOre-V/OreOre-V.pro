3d865f7fdf4a885fc4e5160874b07c745b7aba04
ataga101 <wentup_3one@outlook.jp>
2021-11-17 14:11:55 +0900

    retrieved OreOre-V

---
+++ OreOre-V/OreOre-V.pro
@@ -0,0 +1,44 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++11
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    ../asm/Instructions.cpp \
+    ../asm/Parse.cpp \
+    ../sim/CPU.cpp \
+    ../sim/Memory.cpp \
+    ../sim/Simulator.cpp \
+    ../sim/fpu.cpp \
+    ../sim/util.cpp \
+    main.cpp \
+    mainwindow.cpp \
+    simobj.cpp
+
+HEADERS += \
+    ../asm/Instructions.hpp \
+    ../asm/Parse.hpp \
+    ../sim/CPU.hpp \
+    ../sim/Memory.hpp \
+    ../sim/Simulator.hpp \
+    ../sim/fpu.hpp \
+    ../sim/util.hpp \
+    mainwindow.h \
+    simobj.h
+
+FORMS += \
+    mainwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+DISTFILES += \
+    ../asm/.gitignore \
+    ../asm/README.md
---
+++ OreOre-V/mainwindow.h
@@ -0,0 +1,60 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QThread>
+#include "../sim/Simulator.hpp"
+#include "simobj.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_7_released();
+
+    void on_pushButton_8_released();
+
+    void on_pushButton_9_released();
+
+    void on_address_textChanged(const QString &arg1);
+
+    void on_Instructions_cellClicked(int row, int column);
+
+    void on_pushButton_released();
+
+    void on_pushButton_2_released();
+
+    void on_pushButton_3_clicked();
+
+    void refreshAll();
+
+    void on_InstLinespinBox_valueChanged(int arg1);
+
+private:
+    Ui::MainWindow *ui;
+    QThread simThread;
+    void SetTableWidth();
+    void resizeEvent(QResizeEvent* event);
+    void refreshRegView();
+    void refreshMemView();
+    void refreshInstView();
+    int mem_addr;
+    int inst_line;
+    bool isReghex;
+    bool running;
+    simObj sobj;
+
+signals:
+    void tellSimRun();
+    void tellSimRead();
+};
+#endif // MAINWINDOW_H
