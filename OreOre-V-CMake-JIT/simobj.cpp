d3c426cde29dc5bc626cec3f12b819ef51b65394
ataga101 <wentup_3one@gmail.com>
2021-12-30 00:45:48 +0900

    Added Error Notification

--- OreOre-V-CMake-JIT/simobj.cpp
+++ OreOre-V-CMake-JIT/simobj.cpp
@@ -1,4 +1,7 @@
 #include "simobj.h"
+#include <QApplication>
+#include <QMessageBox>
+#include <sstream>
 
 simObj::simObj(QObject *parent) : QObject(parent)
 {
@@ -6,14 +9,39 @@ simObj::simObj(QObject *parent) : QObject(parent)
 }
 
 void simObj::run(){
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+
     if(needReset) sim.reset();
-    int ret = sim.run();
-    needReset = (ret == 0) ? true : false;
+
+    try{
+        int ret = sim.run();
+        needReset = (ret == 0) ? true : false;
+        QApplication::setOverrideCursor(Qt::ArrowCursor);
+    }catch(std::exception &e){
+        QApplication::setOverrideCursor(Qt::ArrowCursor);
+        stringstream err;
+        err << "Error Occured!" << endl << "Error: " << e.what() << endl << "Aborting" << endl;
+        QMessageBox errBox;
+        errBox.setText(err.str().data());
+        errBox.exec();
+    }
+
     Q_EMIT finished();
 }
 
 void simObj::read(){
-    sim.read_asm(filename);
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    try{
+        sim.read_asm(filename);
+        QApplication::setOverrideCursor(Qt::ArrowCursor);
+    }catch(std::exception &e){
+        QApplication::setOverrideCursor(Qt::ArrowCursor);
+        stringstream err;
+        err << "Error Occured!" << endl << "Error: " << e.what() << endl << "Aborting" << endl;
+        QMessageBox errBox;
+        errBox.setText(err.str().data());
+        errBox.exec();
+    }
     Q_EMIT finished();
 }
 
