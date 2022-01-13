51dff48dafff701e51bedf073af0c922eae9d455
ataga101 <wentup_3one@gmail.com>
2022-01-13 16:03:43 +0900

    added CMakeLists.txt

---
+++ lib/main.cpp
@@ -0,0 +1,30 @@
+#include "DisAssembler.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    string filename(argv[1]);
+    std::ifstream test(filename); 
+    if (!test)
+    {
+        std::cout << "The file \"" << filename << "\" doesn't exist" << std::endl;
+        return -1;
+    }
+
+    fstream input;
+
+    input.open(filename, ios::in);
+
+    int code;
+    int now_addr = 0;
+
+    while(input.read((char*) &code, sizeof(int)))
+    {
+        cout << now_addr << ": ";
+        cout << disassemble(code) << endl;
+    }
+
+    input.close();
+}
