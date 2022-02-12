e60081f9fe881a7076871e12290479c35f7efd1a
ataga101 <wentup_3one@gmail.com>
2022-02-12 21:28:44 +0900

    fixed srai

--- asm/Parse.cpp
+++ asm/Parse.cpp
@@ -482,6 +482,169 @@ Parse :: Parse(string str, int now_addr)
                 ));
             }
             
+            //1st -> 2nd psuedo instructions
+            else if(match[1].str() == "fli"){
+                float imm = stof(match[3].str());
+                uint32_t* immint = (uint32_t *)&imm;
+                uint32_t luiimm = (*immint) >> 12;
+                uint32_t addiimm = (*immint) & ((1 << 12) - 1);
+                codes.push_back(ILtype(
+                    5,
+                    2,
+                    regname_to_addr("a21"),
+                    ((unsigned int)luiimm >> 16),
+                    luiimm & ((1 << 16) -1)
+                ));
+                codes.push_back(ILtype(
+                    4,
+                    0,
+                    regname_to_addr("a21"),
+                    regname_to_addr("a21"),
+                    addiimm
+                ));
+                codes.push_back(Rtype(
+                    4,
+                    0,
+                    regname_to_addr(match[2].str()),
+                    regname_to_addr("a21"),
+                    0,
+                    0));
+            }else if(match[1].str() == "li"){
+                uint32_t imm = (uint32_t)stoi(match[3].str());
+
+                if(imm >= (1 << 16)){
+                    int luiimm = imm >> 12;
+                    int addiimm = imm & ((1 << 12) - 1);
+                    
+                    codes.push_back(ILtype(
+                        5,
+                        2,
+                        regname_to_addr(match[2].str()),
+                        ((unsigned int)luiimm >> 16),
+                        luiimm & ((1 << 16) -1)
+                    ));
+                    
+                    codes.push_back(ILtype(
+                        4,
+                        0,
+                        regname_to_addr(match[2].str()),
+                        regname_to_addr(match[2].str()),
+                        addiimm
+                    ));
+                }else{
+                    codes.push_back(ILtype(
+                        4,
+                        0,
+                        regname_to_addr(match[2].str()),
+                        regname_to_addr("zero"),
+                        imm
+                    ));
+                }
+                
+            }else if(match[1].str() == "la"){
+                size = 2;
+                int imm = label_to_addr(match[3].str(), 0);
+
+                int luiimm = imm >> 12;
+                int addiimm = imm & ((1 << 12) - 1);
+                
+                codes.push_back(ILtype(
+                    5,
+                    2,
+                    regname_to_addr(match[2].str()),
+                    ((unsigned int)luiimm >> 16),
+                    luiimm & ((1 << 16) -1)
+                ));
+                
+                codes.push_back(ILtype(
+                    4,
+                    0,
+                    regname_to_addr(match[2].str()),
+                    regname_to_addr(match[2].str()),
+                    addiimm
+                ));
+            }else if(match[1].str() ==  "fmv.x.w"){
+                codes.push_back(Rtype(
+                    4,
+                    0,
+                    regname_to_addr(match[2].str()),
+                    regname_to_addr(match[3].str()),
+                    0,
+                    0));
+            }else if(match[1].str() ==  "fmv.w.x"){
+                codes.push_back(Rtype(
+                    4,
+                    0,
+                    regname_to_addr(match[2].str()),
+                    regname_to_addr(match[3].str()),
+                    0,
+                    0));
+            }else if(match[1].str() ==  "fmv"){
+                codes.push_back(Rtype(
+                    4,
+                    0,
+                    regname_to_addr(match[2].str()),
+                    regname_to_addr(match[3].str()),
+                    0,
+                    0));
+            }            
+            else if(match[1].str() ==  "flw"){
+                codes.push_back(ILtype(
+                    5,
+                    0,
+                    regname_to_addr(match[2].str()),
+                    regname_to_addr(match[4].str()),
+                    stoi(match[3].str())
+                ));
+            }else if(match[1].str() ==  "fsw"){
+                codes.push_back(SBtype(
+                    6,
+                    6,
+                    regname_to_addr(match[4].str()),
+                    regname_to_addr(match[2].str()),
+                    stoi(match[3].str())
+                ));
+            }else if(match[1].str() ==  "jal"){
+                int lab = label_to_addr(match[3].str(), 0);
+                codes.push_back(Jtype(
+                    7,
+                    2,
+                    lab
+                ));
+            }else if(match[1].str() ==  "jalr"){
+                int rs1 = regname_to_addr(match[3].str()) & 0b11111;
+                if(match[2].str() == "ra"){
+                    codes.push_back(Jtype(
+                        7,
+                        3,
+                        (rs1 << 20)
+                    ));
+                }else if(match[2].str() == "zero"){
+                    if(match[3].str() == "swp"){
+                        codes.push_back(Jtype(
+                            7,
+                            1,
+                            (rs1 << 20)
+                        ));
+                    }else if(match[3].str() == "ra"){
+                        codes.push_back(Jtype(
+                            7,
+                            4,
+                            0
+                        ));
+                    }
+                }                
+            }else if(match[1].str() ==  "srli"){
+                codes.push_back(ILtype(
+                    4,
+                    2,
+                    regname_to_addr(match[2].str()),
+                    regname_to_addr(match[3].str()),
+                    stoi(match[4].str()) & 0b11111
+                ));
+                
+            }
+
             else{
                 cerr << "Unknown Opecode: " << match[1].str() << endl;
                 exit(1);
