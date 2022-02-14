5d6eb7dc9b0f98d79e020b02df6d1a11a8470b61
ataga101 <wentup_3one@gmail.com>
2022-02-14 16:33:44 +0900

    untracked files on main: 09c4ba0 update packer

---
+++ sim/heyhey.cpp
@@ -0,0 +1,14 @@
+Result Summary
+Clock count: 861144993
+
+ Statistics
+        Number of Instructions:                         502652242
+        Number of 1 cycle stalls (non-memory-access):   12343850
+        Number of 2 cycle stalls (non-memory-access):   41351572
+        Number of 3 cycle stalls (non-memory-access):   487205
+        Number of data hazards:                         43186804
+        Number of branch prediction miss:               48089939
+        Number of cache miss:                           3503356
+        Cache valid rate:                               1
+        Cache hit rate:                                 0.984441
+        Cache replace rate:                             0.015541
