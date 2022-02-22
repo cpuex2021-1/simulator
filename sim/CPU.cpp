ea86e9b45ca07a814163209da9eed8021479801d
ataga101 <wentup_3one@gmail.com>
2022-02-22 14:16:01 +0900

    fixed time prediction

--- sim/CPU.cpp
+++ sim/CPU.cpp
@@ -106,7 +106,7 @@ void CPU::update_clkcount(){
 
 double CPU::get_estimated_time(){
     double time = clk / 50000000.0;
-    uint64_t totaluartComm = instructions.size() * 4 + mem.uart.getInbufSize() + mem.uart.getOutbufIdx();
+    uint64_t totaluartComm = instructions.size() * 4 + mem.uart.getInbufSize(); // + mem.uart.getOutbufIdx();
     time += (totaluartComm / 11520.0);
     return time;
 }
